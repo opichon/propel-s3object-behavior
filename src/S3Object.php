a5215253d32f32eb86d6efcdfba4ff0d46425720
Olivier Pichon <op@united-asian.com>
2013-05-19 16:00:08 +0700

    upgraded aws/aws-sdk package to 2.3

---
+++ src/S3Object.php
@@ -0,0 +1,41 @@
+<?php
+
+interface S3Object
+{
+    /**
+     *
+     * The region in which this object stores its document in AWS S3.
+     *
+     * @return string
+     */
+    public function getRegion();
+
+    /**
+     *
+     * The bucket in which this object stores its document in AWS S3.
+     *
+     * @return string
+     */
+    public function getBucket();
+
+    /**
+     * The key under which this object stores its document in AWS S3.
+     *
+     * @ return string
+     */
+    public function getKey();
+
+    /**
+     * Whether this objects uses server-side encryption to store its document in AWS S3.
+     *
+     * @return Boolean
+     */
+    public function getServerSideEncryption();
+
+    /**
+     * Whether this object uses reduced redundancy storage to store its document in AWS S3.
+     *
+     * @return Boolean
+     */
+    public function getReducedRedundancyStorage();
+}
